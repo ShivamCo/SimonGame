7f0e01baac13102d89a2d99da3ef42141ad91fe2
Shivam <shivam.singh.coo@gmail.com>
2023-07-27 15:26:56 +0530

    Updating JavaScript

--- index.js
+++ index.js
@@ -1,5 +1,94 @@
-$(".button").on("click",function(){
+const buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+
+console.log(userClickedPattern);
+console.log(gamePattern)
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
     
-    alert($(".button").attr("class"))
+    if (!started) {
+        
+      //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+      $("h1").text("Level " + level);
+      nextSequence();
+      started = true;
+    }
+  });
+
+
+
+function nextSequence() {
+
+    level++
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
     
+    gamePattern.push(randomChosenColour);
+    var ids = "#" + randomChosenColour;
+    $(ids).ready(function () {
+        $(ids).fadeOut(50).fadeIn(50).fadeOut(50).fadeIn(50);
+        var audio = new Audio("sounds/" + randomChosenColour + ".mp3")
+    
+        audio.play()
+    })
+
+    $("h1").text("Level " + level)
+
+}
+
+
+
+
+
+//  $(ids).ready(function(){
+//      setInterval(() => {
+//          $(ids).fadeIn();
+//          $(ids).fadeOut();
+//      },100);
+
+//  })
+
+
+// $(ids).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+
+
+$("button").click(function () {
+    
+    
+    var userChosenColour = $(this).attr('id');
+
+    var audio = new Audio("sounds/" + userChosenColour + ".mp3")
+    audio.play()
+    
+    userClickedPattern.push(String(userChosenColour))
+
+    $(this).addClass("hoverEffect")
+
+    setTimeout(function () {
+        $("button").removeClass('hoverEffect').addClass(this.id);
+    }, 100);
+    checkAnswer(userClickedPattern.length - 1);
+
 })
+
+
+
+function checkAnswer(currentLevel){
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        console.log("success")
+        nextSequence().delay(1000)
+    }
+
+    else {
+        console.log("wrong")
+    }
+
+
+};
